27843d1b25d739302a13007a0f064cfe83d48c29
mizo-y@yandex.ru <G!tl@bsk!ll>
2023-12-18 10:00:53 +0100

    Сделано.

--- module_05_processes_and_threads/homework/hw3/block_errors.py
+++ module_05_processes_and_threads/homework/hw3/block_errors.py
@@ -9,10 +9,10 @@ from types import TracebackType
 
 class BlockErrors:
     def __init__(self, errors: Collection) -> None:
-        ...
+        self.errors = errors
 
     def __enter__(self) -> None:
-        ...
+        pass
 
     def __exit__(
             self,
@@ -20,4 +20,35 @@ class BlockErrors:
             exc_val: BaseException | None,
             exc_tb: TracebackType | None
     ) -> Literal[True] | None:
-        ...
+        if exc_type is not None and issubclass(exc_type, tuple(self.errors)):
+            return True # верное исключение есть и подавлено
+
+
+if __name__ == '__main__':
+
+    err_types = {ZeroDivisionError, TypeError}
+    with BlockErrors(err_types):
+        a = 1 / 0
+    print('Выполнено без ошибок')
+    # # print('Выполнено без ошибок')
+
+    # outer_err_types = {TypeError}
+    # with BlockErrors(outer_err_types):
+    #     inner_err_types = {ZeroDivisionError}
+    #     with BlockErrors(inner_err_types):
+    #         a = 1 / '0'
+    #     print('Внутренний блок: выполнено без ошибок')
+    # print('Внешний блок: выполнено без ошибок')
+    # # Внешний блок: выполнено без ошибок
+
+    # err_types = {Exception}
+    # with BlockErrors(err_types):
+    #     a = 1 / '0'
+    # print('Выполнено без ошибок')
+    # # Выполнено без ошибок
+
+    # err_types = {ZeroDivisionError}
+    # with BlockErrors(err_types):
+    #     a = 1 / '0'
+    # print('Выполнено без ошибок')
+    # # TypeError: unsupported operand type(s) for /: 'int' and 'str'
--- module_05_processes_and_threads/homework/hw5_add/self_printing.py
+++ module_05_processes_and_threads/homework/hw5_add/self_printing.py
@@ -2,9 +2,15 @@
 Напишите код, который выводит сам себя.
 Обратите внимание, что скрипт может быть расположен в любом месте.
 """
+import sys
+
 
 result = 0
 for n in range(1, 11):
     result += n ** 2
 
+
 # Secret magic code
+with open(sys.argv[0]) as f:
+    code = f.read()
+    print(code)
